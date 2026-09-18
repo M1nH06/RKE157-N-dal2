2b5182ff768fef256891ed2600ef8d5ef91a74ec
Min-Ho Kim <min-ho.kim@tktk.ee>
2026-09-18 18:47:26 +0300

    Add week 2 files

---
+++ greeting.py
@@ -0,0 +1,19 @@
+#Programmi algus
+#Programm küsib kasutajalt tema perekonnanime
+#Kasutaja sisestab oma perekonnanime
+#Programm küsib kasutajalt tema sugu, sisestades kas "m" või "n"
+#Kasutaja sisestab tähe vastavalt tema soole
+#Kui kasutaja on mees, siis annab programm väljundiks "Tere, härra [Perekonnanimi]!"
+#Kui kasutaja on naine, siis annab programm väljundiks "Tere, proua [Perekonnanimi]!"
+#Kui kasutaja sisestab midagi muud, siis programm annab väljundiks "Tere tulemast, [Perekonnanimi]"
+#Programmi lõpp
+
+perekonnanimi = input("Tere. Mis on Teie perekonnanimi? ")
+sugu = input("Mis on Teie sugu? Sisestage 'm', kui olete meessoost või 'n', kui olete naissoost. ")
+
+if sugu == "m":
+    print(f"Tere, härra {perekonnanimi}!")
+elif sugu == "n":
+    print(f"Tere, proua {perekonnanimi}!")
+else:
+    print(f"Tere tulemast, {perekonnanimi}!")
---
+++ water_intake_calc.py
@@ -0,0 +1,23 @@
+#Programmi algus
+#Määrame, et soovituslik vee joomise päevakogus on 2l ehk 2000 ml
+#Määrame, et üks klaas on 250 ml
+#Programm küsib kasutajalt, mitu klaasi vett on ta juba joonud
+#Kasutaja sisestab arvu
+#Programm arvutab valemi ([klaaside_arv]*250/2000)*100%
+#Kui saadud protsent on alla 50, siis programm väljastab lause "Joo rohkem vett, su keha vajab seda :)!"
+#Kui protsent on alla 100 aga üle 50, siis programm väljastab "Tubli! Jätka samas vaimus ;)!"
+#Kui protsent on 100 või rohkem, siis programm väljastab "Suurepärane, oled joonud piisavalt vett :D!"
+#Kui kasutaja sisestab midagi muud kui arvu, siis programm väljastab "Ma kahjuks ei saa sellest aru :(."
+#Programmi lõpp
+
+eesmärk = 2000
+üks_klaas = 250
+
+kogus = int(input("Kui mitu klaasi vett oled joonud täna? "))
+protsent = (kogus*250/2000)*100
+if protsent < 50:
+    print("Joo rohkem vett, su keha vajab seda! :)")
+elif protsent < 100:
+    print("Tubli! Jätka samas vaimus! ;)")
+else:
+    print("Suurepäerane! Oled joonud piisavalt vett täna. :D")
