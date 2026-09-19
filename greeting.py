0e51cdb9e0c3d6c280fe5a71885a214d0c66e9cf
Min-Ho Kim <min-ho.kim@tktk.ee>
2026-09-19 12:59:38 +0300

    Add week 2 files

--- greeting.py
+++ greeting.py
@@ -1,8 +1,10 @@
 #Programmi algus
 #Programm küsib kasutajalt tema perekonnanime
 #Kasutaja sisestab oma perekonnanime
+#Programm salvestab kasutaja perekonnanime muutujasse “perekonnanimi”
 #Programm küsib kasutajalt tema sugu, sisestades kas "m" või "n"
 #Kasutaja sisestab tähe vastavalt tema soole
+#Programm salvestab kasutaja sisestatud tähe muutujasse “sugu”
 #Kui kasutaja on mees, siis annab programm väljundiks "Tere, härra [Perekonnanimi]!"
 #Kui kasutaja on naine, siis annab programm väljundiks "Tere, proua [Perekonnanimi]!"
 #Kui kasutaja sisestab midagi muud, siis programm annab väljundiks "Tere tulemast, [Perekonnanimi]"
--- water_intake_calc.py
+++ water_intake_calc.py
@@ -1,13 +1,13 @@
 #Programmi algus
-#Määrame, et soovituslik vee joomise päevakogus on 2l ehk 2000 ml
-#Määrame, et üks klaas on 250 ml
+#Määrame, et soovituslik vee joomise päevakogus on 2l ehk 2000 ml muutujasse "eesmärk"
+#Määrame, et üks klaas on 250 ml muutujasse "üks_klaas"
 #Programm küsib kasutajalt, mitu klaasi vett on ta juba joonud
 #Kasutaja sisestab arvu
-#Programm arvutab valemi ([klaaside_arv]*250/2000)*100%
+#Programm salvestab kasutaja sisestatud arvu muutujasse "kogus"
+#Programm arvutab valemi ([klaaside_arv]*250/2000)*100% järgi ning salvestab saadud protsendi muutujasse "protsent"
 #Kui saadud protsent on alla 50, siis programm väljastab lause "Joo rohkem vett, su keha vajab seda :)!"
 #Kui protsent on alla 100 aga üle 50, siis programm väljastab "Tubli! Jätka samas vaimus ;)!"
-#Kui protsent on 100 või rohkem, siis programm väljastab "Suurepärane, oled joonud piisavalt vett :D!"
-#Kui kasutaja sisestab midagi muud kui arvu, siis programm väljastab "Ma kahjuks ei saa sellest aru :(."
+#Kui protsent on 100 või rohkem, siis programm väljastab "Suurepärane, oled joonud piisavalt vett :D!" 
 #Programmi lõpp
 
 eesmärk = 2000
