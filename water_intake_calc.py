c4af94fd41a99064c3b39b5862e9496debd9e23c
Min-Ho Kim <min-ho.kim@tktk.ee>
2026-09-19 13:59:03 +0300

    Add week 2 files

--- water_intake_calc.py
+++ water_intake_calc.py
@@ -1,20 +1,16 @@
-#Programmi algus
-#Määrame, et soovituslik vee joomise päevakogus on 2l ehk 2000 ml muutujasse "eesmärk"
-#Määrame, et üks klaas on 250 ml muutujasse "üks_klaas"
+#Programmi algus 
 #Programm küsib kasutajalt, mitu klaasi vett on ta juba joonud
 #Kasutaja sisestab arvu
 #Programm salvestab kasutaja sisestatud arvu muutujasse "kogus"
-#Programm arvutab valemi ([klaaside_arv]*250/2000)*100% järgi ning salvestab saadud protsendi muutujasse "protsent"
+#Programm arvutab valemi ([klaaside_arv]*250/2000)*100% järgi, kui oletame et üks klaas on 250 ml ja soovitatav kogus on 2 l ehk 2000 ml, ning salvestab saadud protsendi muutujasse "protsent"
 #Kui saadud protsent on alla 50, siis programm väljastab lause "Joo rohkem vett, su keha vajab seda :)!"
 #Kui protsent on alla 100 aga üle 50, siis programm väljastab "Tubli! Jätka samas vaimus ;)!"
 #Kui protsent on 100 või rohkem, siis programm väljastab "Suurepärane, oled joonud piisavalt vett :D!" 
 #Programmi lõpp
-
-eesmärk = 2000
-üks_klaas = 250
-
+ 
 kogus = int(input("Kui mitu klaasi vett oled joonud täna? "))
 protsent = (kogus*250/2000)*100
+
 if protsent < 50:
     print("Joo rohkem vett, su keha vajab seda! :)")
 elif protsent < 100:
